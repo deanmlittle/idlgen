213ef0c9449bc2e4eb94a59a7d3e0a56c6169898
Dean 利迪恩 <dean@bitping.com>
2024-05-02 15:21:13 +0800

    Added pub to account types

--- src/generators/accounts.rs
+++ src/generators/accounts.rs
@@ -20,5 +20,5 @@ pub fn make_accounts(idl: &IDL) -> String {
 }
 
 pub fn make_account_props(account: &Accounts) -> String {
-    account.kind.fields.iter().map(|t| format!("        {}: {}", t.name.to_case(Case::Snake), t.kind.to_string())).collect::<Vec<String>>().join(",\n")
+    account.kind.fields.iter().map(|t| format!("        pub {}: {}", t.name.to_case(Case::Snake), t.kind.to_string())).collect::<Vec<String>>().join(",\n")
 }
