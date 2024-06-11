ae6c70118181749686b025b348ab57ed434f86e1
Dean 利迪恩 <dean@bitping.com>
2024-06-11 18:34:29 +0800

    Fixed events

--- src/generators/events.rs
+++ src/generators/events.rs
@@ -1,9 +1,18 @@
-use crate::{types::Event, IDL};
+use crate::{generators::common::{indent, make_defined_types_fields}, types::Event, IDL};
 use convert_case::{Casing, Case};
 
 pub fn make_events(idl: &IDL) -> String {
     idl.events.iter().map(|event| {
         let event_name_pascal = event.name.to_case(Case::Pascal);
+
+        let fields = match event.fields.is_empty() {
+            true => if let Some(matched_type) = idl.types.iter().find(|t| t.name == event_name_pascal) {
+                        indent(make_defined_types_fields(matched_type.clone()))
+                    } else {
+                        make_event_props(event)
+                    },
+            false => make_event_props(event)
+        };
         
         format!("    #[derive(AnchorDiscriminator, AnchorSerialize, AnchorDeserialize)]
     pub struct {} {{
@@ -16,7 +25,7 @@ pub fn make_events(idl: &IDL) -> String {
             self.serialize(&mut data).unwrap();
             data
         }}
-    }}", event_name_pascal, make_event_props(event), event_name_pascal)
+    }}", event_name_pascal, fields, event_name_pascal)
     }).collect::<Vec<String>>().join("\n\n")
 }
 
